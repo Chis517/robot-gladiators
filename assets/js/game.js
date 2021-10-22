8907d8deb264babd0d1474f9e704d8df4296821b
Chice <cko517@hotmail.com>
2021-10-22 19:28:32 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,6 +19,13 @@ console.log(enemyNames[2]);
 //  * Defeat each enemy-robot
 // "Lose" - Player robot's health is zero or less
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
+
 // fight function
 var fight = function(enemyName) {
   while(playerHealth > 0 && enemyHealth > 0) {
@@ -34,14 +41,16 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + ' has decided to skip this fight. Goodbye!');
       // subtract money from playMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney =  Math.max(0, playerMoney - 10);
       console.log("playerMoney", playerMoney);
       break;
     }
   }
   
-  // remove enemy's health by subtracting the amount set in the playerAttack variable
-  enemyHealth = enemyHealth - playerAttack;
+  // generate random damage value based on player's attack power
+  var damage = randomNumber(playerAttack - 3, playerAttack);
+
+  enemyHealth = Math.max(0, enemyHealth - damage);
   console.log(
     playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
   );
@@ -60,7 +69,7 @@ var fight = function(enemyName) {
     }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - enemyAttack);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -93,7 +102,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // resets enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // passes the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
